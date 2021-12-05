cef6ffc819a9ba1ef57012d6f4bca125781d63cd
Ro`zmat Otajonov <53808777+Otajonov@users.noreply.github.com>
2021-12-05 20:24:39 +0500

    Update troll.py

--- userbot/assistant/troll.py
+++ userbot/assistant/troll.py
@@ -18,13 +18,13 @@ async def on_plug_in_callback_query_handler(event):
             ids = [userid]
             if event.query.user_id in ids:
                 reply_pop_up_alert = (
-                    "You are not allowed to see this message, better luck next time!"
+                    "Bu xabarni siz ko'ra olmaysiz!"
                 )
             else:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in catub server"
+            reply_pop_up_alert = "Bu xabar serverda ortiq mavjud emas"
     else:
-        reply_pop_up_alert = "This message no longer exists "
+        reply_pop_up_alert = "Bu xabar ortiq mavjud emas "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
