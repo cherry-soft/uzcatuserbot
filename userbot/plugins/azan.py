5e4b653326f91bcf8e3bed6246e9001c7ef12a16
Ro`zmat Otajonov <53808777+Otajonov@users.noreply.github.com>
2021-12-05 17:21:33 +0500

    Update azan.py

--- userbot/plugins/azan.py
+++ userbot/plugins/azan.py
@@ -16,30 +16,30 @@ plugin_category = "extra"
     info={
         "header": "Shows you the Islamic prayer times of the given city name.",
         "note": "you can set default city by using {tr}setcity command.",
-        "usage": "{tr}azan <city name>",
-        "examples": "{tr}azan hyderabad",
+        "usage": "{tr}azan <Shahar nomi inglizcha>",
+        "examples": "{tr}azan Kokand",
     },
 )
 async def get_adzan(adzan):
-    "Shows you the Islamic prayer times of the given city name"
+    "Ko'rsatilgan shahar uchun namoz vaqtlari"
     input_str = adzan.pattern_match.group(1)
     LOKASI = gvarstatus("DEFCITY") or "Tashkent" if not input_str else input_str
     url = f"http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc"
     request = requests.get(url)
     if request.status_code != 200:
         return await edit_delete(
-            adzan, f"`Couldn't fetch any data about the city {LOKASI}`", 5
+            adzan, f"`Shahar haqida hech qanday ma'lumot topilmadi {LOKASI}`", 5
         )
     result = json.loads(request.text)
-    catresult = f"<b>Islamic prayer times </b>\
-            \n\n<b>Shahar     : </b><i>{result['query']}</i>\
-            \n<b>Mamlakat  : </b><i>{result['country']}</i>\
-            \n<b>Sana     : </b><i>{result['items'][0]['date_for']}</i>\
-            \n<b>Bomdod     : </b><i>{result['items'][0]['fajr']}</i>\
-            \n<b>Kun chiqishi    : </b><i>{result['items'][0]['shurooq']}</i>\
-            \n<b>Peshin    : </b><i>{result['items'][0]['dhuhr']}</i>\
-            \n<b>Asr    : </b><i>{result['items'][0]['asr']}</i>\
-            \n<b>Shom    : </b><i>{result['items'][0]['maghrib']}</i>\
-            \n<b>Hufton     : </b><i>{result['items'][0]['isha']}</i>\
+    catresult = f"<b>Namoz vaqtlari </b>\
+            \n\n<b>Shahar ---: </b><i>{result['query']}</i>\
+            \n<b>Mamlakat ---: </b><i>{result['country']}</i>\
+            \n<b>Sana -------: </b><i>{result['items'][0]['date_for']}</i>\
+            \n<b>Bomdod -----: </b><i>{result['items'][0]['fajr']}</i>\
+            \n<b>Kun chiqishi: </b><i>{result['items'][0]['shurooq']}</i>\
+            \n<b>Peshin -----: </b><i>{result['items'][0]['dhuhr']}</i>\
+            \n<b>Asr --------: </b><i>{result['items'][0]['asr']}</i>\
+            \n<b>Shom -------: </b><i>{result['items'][0]['maghrib']}</i>\
+            \n<b>Hufton -----: </b><i>{result['items'][0]['isha']}</i>\
     "
     await edit_or_reply(adzan, catresult, "html")
